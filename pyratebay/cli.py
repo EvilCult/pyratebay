147a3a5dde75501ca825217879a6ff00cbe49d72
EvilCult <EvilCult@users.noreply.github.com>
2025-12-12 22:31:08 +0800

    基础程序框架

---
+++ pyratebay/cli.py
@@ -0,0 +1,33 @@
+import argparse
+from pyratebay.main import search_command, info_command, get_command, hot_command
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="pyratebay, a simple command line tool for the pirate bay.")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # search
+    parser_search = subparsers.add_parser("search", help="Search torrent by keyword")
+    parser_search.add_argument("query", type=str, help="Search keyword")
+    parser_search.add_argument("-t", "--type", type=str, default="all", help="Type of content. e.g. movie, tv, music, etc.")
+    parser_search.set_defaults(func=search_command)
+
+    args = parser.parse_args()
+
+    args.func(args)
+
+    # info
+    parser_info = subparsers.add_parser("info", help="Get torrent information by torrent id")
+    parser_info.add_argument("tid", type=int, help="torrent id")
+    parser_info.set_defaults(func=info_command)
+
+    # get
+    parser_get = subparsers.add_parser("get", help="Download torrent")
+    parser_get.add_argument("tid", type=int, help="torrent id")
+    parser_get.set_defaults(func=get_command)
+
+    # hot
+    parser_hot = subparsers.add_parser("hot", help="Get hot torrents")
+    parser_hot.set_defaults(func=hot_command)
+
+if __name__ == "__main__":
+    main()
---
+++ pyratebay/main.py
@@ -0,0 +1,11 @@
+def search_command(args) -> None:
+    pass
+
+def info_command(args) -> None:
+    pass
+
+def get_command(args) -> None:
+    pass
+
+def hot_command() -> None:
+    pass
